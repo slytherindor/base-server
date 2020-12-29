f35477905d96646c5a328b4a27ac5c5f2e8dd0d0
Shubhanshu <shubh.aarsay@gmail.com>
2020-12-29 01:33:50 -0500

    Created index file to start everything.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,17 @@
-import {migrator} from './database/migrator';
-import {User} from './database/models/User';
+import {StorageInitializer} from "./database/StorageInitializer";
+import {ServerInitializer} from "./ServerInitializer";
 
-migrator
-  .up()
-  .then(() => {
-    console.log('MIGRATED');
-  })
-  .catch(err => {
-    console.error(err);
-  });
-User.initialize();
-User.create({
-  firstName: 'Jane',
-  lastName: 'Doe',
-  email: 'janedoe@email.com',
-})
-  .then(userInstance => {
-    console.log(userInstance);
-  })
-  .catch(err => console.error(err));
-User.destroy({where: {id: 2}})
-  .then(r => console.log(r))
-  .catch(err => console.error(err));
+function run(): Promise<void> {
+    return new Promise<void>(async (resolve, reject) => {
+        try {
+            await StorageInitializer.start();
+            ServerInitializer.start();
+
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+}
+
+run().then(r => console.log("Successfully started everything"));

c76898c349efb28b4f366bc5136b7a37108a5711
Shubhanshu <shubh.aarsay@gmail.com>
2021-01-01 02:37:04 -0500

    Added winston logger.

---
+++ src/utils/logger.ts
@@ -0,0 +1,75 @@
+import {LoggerOptions} from 'winston';
+
+const winston = require('winston');
+
+class TimestampFirst {
+  private enabled: boolean;
+  constructor(enabled = true) {
+    this.enabled = enabled;
+  }
+  transform(obj: any) {
+    if (this.enabled) {
+      return Object.assign(
+        {
+          timestamp: obj.timestamp,
+        },
+        obj
+      );
+    }
+    return obj;
+  }
+}
+
+const enumerateErrorFormat = winston.format((info: any) => {
+  if (info.message instanceof Error) {
+    info.message = Object.assign(
+      {
+        message: info.message.message,
+        stack: info.message.stack,
+      },
+      info.message
+    );
+  }
+
+  if (info instanceof Error) {
+    return Object.assign(
+      {
+        message: info.message,
+        stack: info.stack,
+      },
+      info
+    );
+  }
+
+  return info;
+});
+
+const myFormat = winston.format.combine(
+  enumerateErrorFormat(),
+  winston.format.timestamp({
+    format: 'YYYY-MM-DD HH:mm:ss.SSS',
+  }),
+  new TimestampFirst(true),
+  winston.format.json()
+);
+
+const options: LoggerOptions = {
+  transports: [
+    new winston.transports.Console({
+      level: process.env.NODE_ENV === 'production' ? 'error' : 'debug',
+    }),
+    new winston.transports.File({filename: 'debug.log', level: 'debug'}),
+  ],
+  format: myFormat,
+};
+
+const logger = winston.createLogger();
+logger.configure(options);
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.debug('Logging initialized at debug level');
+}
+
+logger.info('This is a test message');
+
+export default logger;

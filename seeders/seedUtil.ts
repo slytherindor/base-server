dfda0125213f42f3a118b041ef21bf7981e64ffa
Shubhanshu <shubh.aarsay@gmail.com>
2021-01-19 18:47:21 -0500

    Created function for creating fake users.

--- seeders/seedUtil.ts
+++ seeders/seedUtil.ts
@@ -2,6 +2,7 @@
 import * as faker from 'faker';
 import {AuthorInterface} from '../src/database/models/Author';
 import {BookInterface} from '../src/database/models/Book';
+import {UserInterface} from '../src/database/models/User';
 
 export function createBooks(numberOfItems: number): BookInterface[] {
   const books: BookInterface[] = [];
@@ -26,4 +27,18 @@ export function createAuthors(numberOfItems: number): AuthorInterface[] {
     authors.push(author);
   }
   return authors;
+}
+
+export function createUsers(numberOfItems: number): UserInterface[] {
+  const users: UserInterface[] = [];
+  for (let i = 1; i <= numberOfItems; i++) {
+    const user: UserInterface = {
+      id: i,
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+    };
+    users.push(user);
+  }
+  return users;
 }

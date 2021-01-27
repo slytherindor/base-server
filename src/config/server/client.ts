eaa7917d95f4f89eafa5f11b58f0eea3228d7df8
Shubhanshu <shubh.aarsay@gmail.com>
2021-01-27 00:36:10 -0500

    Fixing client dir for prod.

--- src/config/server/client.ts
+++ src/config/server/client.ts
@@ -2,5 +2,5 @@ import * as path from 'path';
 import {CLIENT_PATH, prod} from '../../utils/secrets';
 
 export const clientBuildDir = prod
-  ? path.join(__dirname, 'client')
+  ? path.join(__dirname, '..', '..', '..',  'client')
   : path.resolve(CLIENT_PATH!);
